09be73d6ca99ce3cc385adf4f1513cb53740e0bb
jiazhi.sun <jiazhi.sun@mu-c1-16.mullen.pvt.k12.co.us>
2019-02-05 12:23:49 -0700

    Kenny's ArrayList (Not Done)

--- SIS Group Project/src/SISMainMethod.java
+++ SIS Group Project/src/SISMainMethod.java
@@ -1,11 +1,11 @@
-
+import java.io.IOException;
 
 public class SISMainMethod
 	{
 
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 			{
-			// hi 		
+				
 			}
 
 	}
